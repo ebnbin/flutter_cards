166999bbe06caef0191fe1f3d8307a4c44cfcc0f
Ebn Zhang <ebnbin@gmail.com>
2020-03-23 15:18:53 +0800

    Rename

--- lib/data.dart
+++ lib/data.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
 
-part 'data_action.dart';
-part 'data_card.dart';
-part 'data_game.dart';
-part 'data_property.dart';
+part 'data/action.dart';
+part 'data/card.dart';
+part 'data/game.dart';
+part 'data/property.dart';
