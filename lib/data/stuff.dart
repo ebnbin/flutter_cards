99e29e78f27c5c5be0862668a922e2806a93978f
Ebn Zhang <ebnbin@gmail.com>
2020-04-21 19:48:45 +0800

    Update

--- lib/data/stuff.dart
+++ lib/data/stuff.dart
@@ -23,7 +23,7 @@ abstract class _Stuff {
   /// Matrix4.setEntry(3, 2, value). 数值只与尺寸相关.
   double get matrix4Entry32 {
     // 数值越大, 3d 旋转镜头越近, 效果越明显, 但越容易绘制异常.
-    return 0.4 / rect.longSize();
+    return 0.2 / rect.longSize();
   }
 
   double rotateX;
