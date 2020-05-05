828887148b3b39243821bb05a63d9de5abc355cd
Ebn Zhang <ebnbin@gmail.com>
2020-05-06 01:40:09 +0800

    Update

--- lib/data.dart
+++ lib/data.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart' hide Card;
 part 'data/data.dart';
 part 'data/metric.dart';
 part 'data/game.dart';
-part 'data/card_base.dart';
 part 'data/card.dart';
 part 'data/sprite.dart';
 part 'data/util.dart';
